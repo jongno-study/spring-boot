75bc3beb131338372ee09ee7ab09ad72617925ed
heowc <heowc1992@gmail.com>
2018-03-12 23:46:02 +0900

    Add Spring boot guide sample(#2, #3, #4, #5, #6)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# spring-boot
+
+
+## package & execute
+
+```bash
+./mvnw -DskipTests=true package && java -jar target/spring-boot-0.0.1.jar
+``` 
---
+++ src/test/java/com/heowc/springboot/hello/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.heowc.springboot.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
+@WebMvcTest // 자동 구성 비활성화, @Controller, @ControllerAdvice, @JsonComponent, Converter / GenericConverter, Filter, WebMvcConfigurer
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+	}
+}
