3852d3e8f6ebf946df01834ed654204a73fb4b09
chori <slivers721@gmail.com>
2018-03-16 20:45:14 +0900

    Application 클래스 이름 변경

--- src/main/java/com/chori/springboot/Application.java
+++ src/main/java/com/chori/springboot/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
